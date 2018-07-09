0f16ae62099c51253c94a4f1f995175fbd61d0bd
Elvandro <elvisamuni22@gmail.com>
2018-07-09 12:03:13 +0300

    Commit that modified the files.

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,5 @@
-from django.conf.urls import url,include
+from django.conf.urls import url
 from . import views
-from django.contrib import admin
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
