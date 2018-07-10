c01b992af6834a002ebf870c5290d8d802a1e471
Elvandro <elvisamuni22@gmail.com>
2018-07-10 14:45:52 +0300

    Commit to create the database models

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Editor,Article,tags
+
 
 # Create your tests here.
+class EditorTestClass(TestCase):
+    # Set up method
+    def SetUp(self):
+        self.elvis= Editor(first_name = 'elvis', last_name = 'Amuni', email = 'elvisamuni22@gmail.com')
+
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.elvis,Editor))
