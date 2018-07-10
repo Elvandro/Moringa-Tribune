83ea451cff3715580783dfba678cbd4438b04427
Elvandro <elvisamuni22@gmail.com>
2018-07-10 10:50:56 +0300

    Commit to edit the files

--- news/views.py
+++ news/views.py
@@ -6,19 +6,19 @@ import datetime as dt
 def welcome(request):
     return render(request, 'welcome.html')
 
-def news_of_day(request):
-    date = dt.date.today()
+# def news_of_day(request):
+#    date = dt.date.today()
 
     # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1> News for {day}, {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+#    day = convert_dates(date)
+#    html = f'''
+#        <html>
+#            <body>
+#                <h1> News for {day}, {date.day}-{date.month}-{date.year}</h1>
+#            </body>
+#        </html>
+#            '''
+#    return HttpResponse(html)
 
 def news_today(request):
     date =  dt.date.today()
